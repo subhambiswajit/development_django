a7f64d44b0aa1b71f4aaa59e889beb04a4c975d3
subhambiswajit <digu35@gmail.com>
2015-11-29 23:32:21 +0530

    static files work completed

--- bookstore/store/views.py
+++ bookstore/store/views.py
@@ -6,10 +6,13 @@ def index(request):
 	return render(request,'index.html')
 
 def store(request):
+	fb_name = ''
+	head = ''
 	m = Book.objects.all().count()
 	
 	if request.user.is_authenticated():
 		fb_name= request.user.first_name
+		head = 'Welcome to the mystery book'
 	# 	message = 'user is authenticated'
 
-	return render(request,'store.html',{'store' : m ,'facebook_name': fb_name})
+	return render(request,'base.html',{'store' : m ,'facebook_name': fb_name , 'head': head})
